2c148e7c023bc741abdf814627a28ea366877692
The7Even <161558340+The7Even@users.noreply.github.com>
2024-09-10 12:30:08 +0900

    Editing So Much
    That's a lot of Damage!

--- testfile.py
+++ testfile.py
@@ -1,18 +1,15 @@
 def printnumber(x):
-    print(f"{x}는 {x**300}가 됩니다")
+    print(f"{x}는 {x**3}가 됩니다") # 300은 존나커서 3으로 수정했어요
     
     
- a = int(input("숫자를 입력. >>>"))
- printnumber(a)
+a = int(input("숫자를 입력. >>>")) # 웬 이상한 들여쓰기가
+printnumber(a)
  
- b = int(input("ㅅㅈㄹ ㅇㄹ. >>>"))
- c = int(input("ㅜㅏㅡ ㅣㅕ. >>>"))
+b = int(input("ㅅㅈㄹ ㅇㄹ. >>>"))
+c = int(input("ㅜㅏㅡ ㅣㅕ. >>>"))
  
- print(f"b + c = {b+c}")
- a = 0
+print(f"b + c = {b+c}")
+a = 0
 
 for i in range(1000):
         print(f"{i+1}회 실행됨.")
-        a =+ i+1
-
-print(a)
