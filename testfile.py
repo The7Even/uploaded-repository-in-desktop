7b7dc7cda8f86c1b4832957efe7050db7837f0a1
The7Even <161558340+The7Even@users.noreply.github.com>
2024-09-10 12:26:01 +0900

    strange lines added XD
    Commit message changed!
    
    Such English
    Much Typing
    Wow

--- testfile.py
+++ testfile.py
@@ -1,5 +1,18 @@
 def printnumber(x):
-    print(f"{x}는 {x**2}가 됩니다")
+    print(f"{x}는 {x**300}가 됩니다")
+    
     
  a = int(input("숫자를 입력. >>>"))
  printnumber(a)
+ 
+ b = int(input("ㅅㅈㄹ ㅇㄹ. >>>"))
+ c = int(input("ㅜㅏㅡ ㅣㅕ. >>>"))
+ 
+ print(f"b + c = {b+c}")
+ a = 0
+
+for i in range(1000):
+        print(f"{i+1}회 실행됨.")
+        a =+ i+1
+
+print(a)
